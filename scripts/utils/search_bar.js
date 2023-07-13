1c3cb9f0ea4fc6c32e6ed009cbeb7303bbfec9cd
TheGrandLynx <terence.ngama@gmail.com>
2023-07-13 09:43:27 +0200

    Optimisation du code

--- scripts/utils/search_bar.js
+++ scripts/utils/search_bar.js
@@ -1,57 +1,24 @@
 let arrayRecettesAffiches = [];
 
+//filtrer les recettes à afficher en fonction du texte tapé dans la barre de recherche
 function rechercherRecette(){
     const searchBar = document.querySelector('.searchRechercherRecette');
-    
-    let wArrayRecettesAffiches;
     let strTexteRecherche = searchBar.value;
-    //if(strTexteRecherche.length <= 2){return;}
     strTexteRecherche = strTexteRecherche.toLowerCase();
-   
-    //console.log(wArticles)
-    /* arrayArticlesAffiches = wArticles.filter(article => article.getElementsByTagName('article')[0].getElementsByTagName('h2')[0].textContent.toLowerCase().includes(strTexteRecherche)
-                                                        || article.getElementsByTagName('article')[0].getElementsByClassName('pRecette')[0].textContent.toLowerCase().includes(strTexteRecherche)
-                                                        || isIncludedIn(strTexteRecherche, article.getElementsByTagName('article')[0].getElementsByTagName('h4')))
-    arrayArticlesNonAffiches = wArticles.filter(article => !article.getElementsByTagName('article')[0].getElementsByTagName('h2')[0].textContent.toLowerCase().includes(strTexteRecherche)
-                                                        && !article.getElementsByTagName('article')[0].getElementsByClassName('pRecette')[0].textContent.toLowerCase().includes(strTexteRecherche)
-                                                        && !isIncludedIn(strTexteRecherche, article.getElementsByTagName('article')[0].getElementsByTagName('h4')))
-    
-    
-    arrayArticlesAffiches.map(article => article.classList.remove('disparition'));
-    arrayArticlesNonAffiches.map(article => article.classList.add('disparition')); */
-    //console.log(currentRecipes)
-   
-                                                             //arrayRecettesNonAffiches = currentRecipes.filter(recette => !recette["name"].toLowerCase().includes(strTexteRecherche)
-     //                                                        && !recette["description"].toLowerCase().includes(strTexteRecherche)
-      //                                                       && !isIncludedIn(strTexteRecherche, recette["ingredients"]))
-    
-    console.log(arrayRecettesAffiches)
-    //console.log(currentRecipes[0].ingredients[0].ingredient)
-    //arrayRecettesAffiches = currentRecipes.filter(recette => recette.ingredients.ingredient.toLowerCase().includes(strTexteRecherche))
-    //arrayRecettesAffiches = currentRecipes.filter(recette => isIncludedIn(strTexteRecherche, recette["ingredients"]))
-    //arrayRecettesAffiches = currentRecipes.filter(recette => recette["ingredients"].flat(Infinity).includes(strTexteRecherche))
-    //arrayRecettesAffiches = currentRecipes.filter(recette => test(recette["ingredients"]))
-    //console.log(arrayRecettesAffiches)
-    //console.log(arrayRecettesAffiches.map(({ id }) => id))
-    //wArticles.map(article => article.id.includes(arrayRecettesAffiches.map(({ id }) => id)))
-    
     if(strTexteRecherche.length > 2){
-        console.log('supérieur')
         arrayRecettesAffiches = arrayRecettesAffiches.filter(recette => recette["nameMin"].includes(strTexteRecherche)
                                                                  || recette["descriptionMin"].includes(strTexteRecherche)
                                                                  || isIncludedIn(strTexteRecherche, recette["ingredientsMin"], true))
+        
+        
         }
-
-   
+        noRecipe(strTexteRecherche);
 }
 
+//afficher les cards correspondants aux recettes à afficher
 function updateArticles(){
-    
     let articles = document.querySelectorAll('.cardRecette');
-    //console.log(articles)
     const wArticles = [...articles];
-   
-
     wArticles.map(article => {if(arrayRecettesAffiches.map(({ id }) => `card${id}`).includes(article.id)){
                                 article.classList.remove('disparition')}
                                 else{
@@ -61,28 +28,22 @@ function updateArticles(){
     
 }
 
+//filtrer les recettes à afficher en fonction des tags
 function rechercherRecetteTag(){
+    let wArrayRecettesAffiches;
     if(tagAffiches.length>0){
-        /*  console.log(tagAffiches.filter(tag => tag[1] == 'Ingrédients')[0][2])
-         wArrayRecettesAffiches = currentRecipes.filter(recette => tagIncluded(tagAffiches.filter(tag => tag[1] == 'Ingrédients'), recette['ingredients']) 
-             
-             ) */
              wArrayRecettesAffiches = currentRecipes;
-             if(tagAffiches.filter(tag => tag[1] ==  'Ingrédients').length>0){
-                 console.log('ingredient')
-             tagAffiches.filter(tag => tag[1] ==  'Ingrédients').map(tag => tag[2]).forEach(tag =>
-                 wArrayRecettesAffiches = wArrayRecettesAffiches.filter(recette => recette.ingredients.map(ing => ing.ingredient).flat(1).includes(tag)))
+             if(tagAffiches.filter(tag => tag[1] ==  'Ingredients').length>0){
+             tagAffiches.filter(tag => tag[1] ==  'Ingredients').map(tag => tag[2]).forEach(tag =>
+                 wArrayRecettesAffiches = wArrayRecettesAffiches.filter(recette => recette.ingredientsMin.map(ing => ing.ingredient).flat(1).includes(tag)))
              }
              if(tagAffiches.filter(tag => tag[1] ==  'Ustensiles').length>0){
-                 console.log('ustensile')
                  tagAffiches.filter(tag => tag[1] ==  'Ustensiles').map(tag => tag[2]).forEach(tag =>
-                     wArrayRecettesAffiches = wArrayRecettesAffiches.filter(recette => recette.ustensils.flat(1).includes(tag)))
+                     wArrayRecettesAffiches = wArrayRecettesAffiches.filter(recette => recette.ustensilsMin.flat(1).includes(tag)))
                  }
                  if(tagAffiches.filter(tag => tag[1] ==  'Appareils').length>0){
-                     console.log('appareils')
-                     console.log(tagAffiches.filter(tag => tag[1] ==  'Appareils').map(tag => tag[2]))
                      tagAffiches.filter(tag => tag[1] ==  'Appareils').map(tag => tag[2]).forEach(tag =>
-                         wArrayRecettesAffiches = wArrayRecettesAffiches.filter(recette => tag.includes(recette['appliance'])))
+                         wArrayRecettesAffiches = wArrayRecettesAffiches.filter(recette => tag.includes(recette['appliance'].toLowerCase())))
                      }
      
      }else{
@@ -91,32 +52,15 @@ function rechercherRecetteTag(){
      arrayRecettesAffiches = wArrayRecettesAffiches;
 }
 
-function test(wTab){
-    console.log(wTab)
-    if(wTab.length > 4 ){
-        return true;
-    }
-}
-
+//rechercher si le texte tapé est présent dans les ingrédients d la recette
 function isIncludedIn(wStringToSearch, wTab, isIngredient){
-   /*  wTab.forEach(wIngredient => {
-        //console.log(wIngredient["ingredient"])
-        if(wIngredient["ingredient"].toLowerCase().includes(wStringToSearch)){
-            console.log(wTab)
-            console.log(wIngredient["ingredient"])
-            console.log(true)
-            return true;
-        }
-    }); */
     for(j = 0; j < wTab.length; j++){
-        console.log(isIngredient)
         if(isIngredient){
             if(wTab[j].ingredient.includes(wStringToSearch)){
                 return true;
             }
         }
         else{
-            console.log(wTab[j])
             if(wTab[j].includes(wStringToSearch)){
                 return true;
             }
@@ -124,15 +68,30 @@ function isIncludedIn(wStringToSearch, wTab, isIngredient){
         }   
     }
 
+    /* function isIncludedIn(wStringToSearch, wTab, isIngredient){
+                wTab.map(ing => ing.ingredient).forEach(ingredient => {
+                    if(ingredient.includes(wStringToSearch)){
+                    console.log(ingredient)
+                    return true;
+                }})
+        } */
 
 function tagIncluded(wTag, wTab){
     for(t = 0; t < wTab.length; t++){
-        console.log(wTag.map(tag=> tag[2]))
-        console.log(wTab[t][0])
-        //console.log(wTag.map(tag => tag[0][2]))
         if(wTag.map(tag => tag[2]).includes(wTab[t].ingredient)){
             return true;
         }
+    }
+}
+
+//afficher le message si aucune recette ne correspond à la recherche
+function noRecipe(strTexteRecherche){
+    const pNoRecipe = document.querySelector('.pRecetteAucune');
+    if(arrayRecettesAffiches.length>0){
+        pNoRecipe.classList.add('disparition');
+    }else{
+        pNoRecipe.innerHTML = `Aucune recette ne contient '${strTexteRecherche}'`;
+        pNoRecipe.classList.remove('disparition');
     }
 }
 
--- scripts/utils/search_tag.js
+++ scripts/utils/search_tag.js
@@ -3,50 +3,49 @@ let currentAppareils;
 let currentUstensils;
 let tagAffiches = [];
 let listTags = [];
-function addClickListener(wContainer, wSearch){
+const listType =  ['Ingredients', 'Appareils', 'Ustensiles']
+
+//ajouter les différents listeners necessaires
+function addClickListener(wContainer, wSearch, wType){
     wContainer.addEventListener('click', function(){
         wContainer.parentNode.classList.toggle('divContainerOptionActive');
         wContainer.children[1].classList.toggle('rotateImage');
     });
-    console.log(wContainer.parentNode)
     wContainer.parentNode.addEventListener('focusout', function(event){
-        console.log(event.currentTarget)
         if(!event.currentTarget.contains(event.relatedTarget)){
             wContainer.parentNode.classList.remove('divContainerOptionActive');
         }
     })
     wSearch.addEventListener("keyup", function(){
-        rechercherTag(wSearch);
+        rechercherTag(wSearch, wType);
     })
 }
 
-function getIngredientsList(globalArrayIngredient, wType){
-    let ingredientList = [];
-    globalArrayIngredient.forEach(arrayIngredient => {
-        arrayIngredient.forEach(ingredient => ingredientList.push([ingredient, wType]));
+//retourne un tableau dans la liste des tags avec le nom du tag et le type de tag
+function getTagsList(globalArrayTag, wType){
+    let tagList = [];
+    globalArrayTag.forEach(arrayTag => {
+        arrayTag.forEach(tag => tagList.push([tag.toLowerCase(), wType]));
     });
     
-    console.log(ingredientList)
-    let wArray = arrayUnique(ingredientList);
-    console.log(ingredientList)
+    let wArray = arrayUnique(tagList);
     wArray.sort(function (a, b) {
         return a[0].localeCompare(b[0]);
     });
     return (wArray)
 }
 
-function populateIngredientList(globalArrayIngredient, wType){
+
+//Remplir la liste des tags disponibles pour ingrédients, appareils et ustensils
+function populateTagList(globalArrayTag, wType){
     let wCurrentIngredients;
-    wCurrentIngredients = getIngredientsList(globalArrayIngredient, wType);
+    wCurrentIngredients = getTagsList(globalArrayTag, wType);
     wCurrentIngredients.forEach(tag => currentIngredients.push(tag));
-    //currentIngredients.push(getIngredientsList(globalArrayIngredient, wType));
-    console.log(currentIngredients)
     wCurrentIngredients = currentIngredients.filter(tag => tag[1] == wType);
-    console.log(wCurrentIngredients)
     const wListOptions = document.querySelectorAll('.listOptions');
     let listOptions;
     switch (wType) {
-        case 'Ingrédients':
+        case 'Ingredients':
         listOptions = wListOptions[0];
         break;
         case 'Appareils':
@@ -60,17 +59,17 @@ function populateIngredientList(globalArrayIngredient, wType){
 
     wCurrentIngredients.forEach(ingredient => {
         let li = document.createElement("li");
-        li.setAttribute('id', ingredient[0].toLowerCase().replace(/\s+/g, ''))
+        li.setAttribute('id', `${wType}${ingredient[0].replace(/\s+/g, '')}`)
         li.innerText = ingredient[0];
         li.setAttribute("tabindex", `0`);
         li.addEventListener('click', function(e){
             addTag(ingredient[0], wType);
-            console.log(tagAffiches)
         })
         listOptions.appendChild(li);
     })
 }
 
+//retourne un tableau  avec les doublons supprimés
 function arrayUnique(arr){
     var temp = ''
     var unique = arr.sort().filter(r => {
@@ -82,58 +81,50 @@ function arrayUnique(arr){
     return (unique);
 }
 
+//rechercher et afficher les tags correspondants à la recherche
 function rechercherTag(wSearchOption, wType){
-    console.log(wSearchOption)
     let wArrayLI = wSearchOption.parentNode.parentNode.getElementsByTagName('li');
     let arrayLI =  [...wArrayLI];
-    console.log(arrayLI)
     let strTexteRecherche = wSearchOption.value;
     strTexteRecherche = strTexteRecherche.toLowerCase();
     wIngredients = currentIngredients.filter(ingredient => ingredient[0].toLowerCase().includes(strTexteRecherche))
-    console.log(wIngredients)
     
     let wArrayRecettesAffiches;
-    if(arrayRecettesAffiches.length>0){
+    if(arrayRecettesAffiches.length>0 && wType != undefined){
         switch (wType){
-        case 'Ingrédients':
-        wArrayRecettesAffiches = arrayRecettesAffiches.map(recette => recette.ingredients.map(ingredient => ingredient.ingredient));
+        case 'Ingredients':
+        wArrayRecettesAffiches = arrayRecettesAffiches.map(recette => recette.ingredientsMin.map(ingredient => ingredient.ingredient));
         break;
         case 'Appareils':
-        wArrayRecettesAffiches = arrayRecettesAffiches.map(recette => recette.appliance);
+        wArrayRecettesAffiches = arrayRecettesAffiches.map(recette => recette.applianceMin);
             
         break;
         case 'Ustensiles':
-        wArrayRecettesAffiches = arrayRecettesAffiches.map(recette => recette.ustensils);
+        wArrayRecettesAffiches = arrayRecettesAffiches.map(recette => recette.ustensilsMin);
         break;
     }
-        
         wArrayRecettesAffiches = wArrayRecettesAffiches.flat(1);
-        console.log(wArrayRecettesAffiches)
         wIngredients = wIngredients.filter(tag => wArrayRecettesAffiches.includes(tag[0]))
     }
     
-    console.log(wIngredients)
-    console.log(wIngredients.map((ingredient => ingredient[0].replace(/\s+/g, ''))))
-    arrayLI.map(tag => {if(wIngredients.map((ingredient => ingredient[0].toLowerCase().replace(/\s+/g, ''))).includes(tag.id)){
-        console.log('app')
+     arrayLI.map(tag => {if(wIngredients.map((ingredient => `${wType}${ingredient[0].toLowerCase().replace(/\s+/g, '')}`)).includes(tag.id)){
+        
         tag.classList.remove('disparition')}
         else{
-            console.log('disp')
             tag.classList.add('disparition')
         }
     })
 }
 
-function getTagDom(wIngredient){
+//crée le dom du tag en fonction du type
+function getTagDom(wIngredient, wType){
     const pTag = document.createElement('p');        
     pTag.innerHTML = `${wIngredient}`
     const imgTag = document.createElement('img')
     imgTag.setAttribute("src", '/assets/icon/cancel.svg')
     imgTag.setAttribute("alt", ``)
     imgTag.addEventListener('click', function(e){
-        //console.log(this.previousElementSibling.innerText)
-        removeTag(this.previousElementSibling.innerText);
-        //console.log(tagAffiches)
+        removeTag(wType+this.previousElementSibling.innerText);
     })
     const divTag = document.createElement('div');
     divTag.appendChild(pTag);
@@ -142,61 +133,61 @@ function getTagDom(wIngredient){
     const divTagContainer = document.createElement('div');
     divTagContainer.classList.add('disparition');
     divTagContainer.classList.add('divTagContainer');
-    divTagContainer.setAttribute('id', `tag${wIngredient.toLowerCase().replace(/\s+/g, '')}`)
+    divTagContainer.setAttribute('id', `tag${wType}${wIngredient.toLowerCase().replace(/\s+/g, '')}`)
 
     divTagContainer.appendChild(divTag);
     listTags.push(divTagContainer)
     return divTagContainer;
 }
 
+//crée la liste des dom tags et les ajoute dans la section tag
 function populateTagDom(globalArrayIngredient, wType){
     
-    //currentIngredients = getIngredientsList(globalArrayIngredient);
+    //currentIngredients = getTagsList(globalArrayIngredient);
     const sectionTags = document.querySelector('.sectionTags');
-    console.log(currentIngredients)
     currentIngredients.forEach(ingredient =>
-        sectionTags.appendChild(getTagDom(ingredient[0]))
+        sectionTags.appendChild(getTagDom(ingredient[0], ingredient[1]))
         )
-        console.log("listTags")
-        console.log(listTags)
 }
 
+//ajouter un tag à la liste des tags affichés et mettre à jour les élément des tags disponibles
 function addTag(wTag, wType){
-    tagAffiches.push([`tag${wTag}`, `${wType}`, wTag]);
+    tagAffiches.push([`tag${wType}${wTag}`, `${wType}`, wTag]);
     displayTag();
     updateRecettes();
     updateListTags()
     
 }
 
+//enlever un tag à la liste des tags affichés et mettre à jour les élément des tags disponibles
 function removeTag(wTag){
     const wIndex = tagAffiches.indexOf(`tag${wTag}`);
-    console.log('index'+wIndex)
-    //console.log(tagAffiches)
-    //console.log(tagAffiches[wIndex])
     tagAffiches.splice(wIndex, 1);
     displayTag();
     updateRecettes();
     updateListTags();
 }
 
+//mettre à jour les tags disponibles en fonction des tags sélectionnés
 function updateListTags(){
     const arraySearchOptions = document.querySelectorAll('.searchOption')
-    const listType =  ['Ingrédients', 'Appareils', 'Ustensiles']
+    
     for(i=0; i<arraySearchOptions.length; i++){
         rechercherTag(arraySearchOptions[i], listType[i]);
 
     }
 }
 
+
+//afficher un tag
 function displayTag(){
-    let wArrayTag = document.querySelectorAll('divTagContainer');
+    let wArrayTag = document.querySelectorAll('.divTagContainer');
     let arrayTag = [...wArrayTag];
-    console.log(arrayTag)
-    console.log(tagAffiches)
-    listTags.map(tag => {if(tagAffiches.map((ingredient => ingredient[0].toLowerCase().replace(/\s+/g, ''))).includes(tag.id)){
+    listTags.map(tag => {if(tagAffiches.map((ingredient => `${ingredient[0].replace(/\s+/g, '')}`)).includes(tag.id)){
+        
         tag.classList.remove('disparition')}
         else{
+        
             tag.classList.add('disparition')
         }
     })
@@ -206,10 +197,9 @@ function init(){
     const divContainer = document.querySelectorAll(".titreOption");
     const searchOption = document.querySelectorAll(".searchOption");
     
-    console.log(divContainer.length)
+    
     for(i=0; i<divContainer.length; i++){
-        console.log(i)
-        addClickListener(divContainer[i], searchOption[i]);
+        addClickListener(divContainer[i], searchOption[i], listType[i]);
     }
     
 }
